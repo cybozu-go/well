4d69a58e26a1b14033702e96a2bcfebafb94904d
ymmt2005 <ymmt2005@gmail.com>
2016-08-18 14:50:26 +0900

    Update docs.

--- doc.go
+++ doc.go
@@ -2,20 +2,22 @@
 Package cmd provides a framework that helps implementation of
 commands having these features:
 
-* Better logging
+Better logging:
 
-  By using github.com/cybozu-go/log package, logs can be structured
-  in JSON or logfmt format.  HTTP servers log accesses automatically.
+By using github.com/cybozu-go/log package, logs can be structured
+in JSON or logfmt format.  HTTP servers log accesses automatically.
 
-* Graceful exit
+Graceful exit:
 
-  The framework provides functions to manage goroutines and
-  network server implementation that can be shutdown gracefully.
+The framework provides functions to manage goroutines and
+network server implementation that can be shutdown gracefully.
 
-* Signal handlers
+Signal handlers:
 
-  The framework installs SIGINT/SIGTERM signal handlers for
-  graceful exit, and SIGUSR1 signal handler to reopen log files.
+The framework installs SIGINT/SIGTERM signal handlers for
+graceful exit, and SIGUSR1 signal handler to reopen log files.
+
+Environment
 
 Environment is the heart of the framework.  It provides a base
 context.Context that will be canceled before program stops, and
