591bae419e9ffe583a22d99a6e443321e60afce2
ymmt2005 <ymmt2005@gmail.com>
2016-08-23 12:47:08 +0900

    [exec] record command execution results.
    This commit implements LogCmd that is a wrapper for *exec.Cmd.
    
    Similar to HTTPCient, LogCmd records command execution results
    and optionally includes the tracking request ID if given context
    has a request ID value.
    
    CommandContext is a function similar to exec.CommandContext, but
    creates and resturns *LogCmd instead of *exec.Cmd.

---
+++ exec_test.go
@@ -0,0 +1,111 @@
+// +build !windows
+
+package cmd
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"testing"
+	"time"
+	"unicode/utf8"
+
+	"github.com/cybozu-go/log"
+)
+
+func TestUTF8StringFromBytes(t *testing.T) {
+	t.Parallel()
+
+	str := "世\xc5\x33\x34"
+	if utf8.ValidString(str) {
+		t.Error(str + " should be invalid")
+	}
+
+	vstr := UTF8StringFromBytes([]byte(str))
+	if !utf8.ValidString(vstr) {
+		t.Error(vstr + ` should be valid`)
+	}
+}
+
+func TestLogCmd(t *testing.T) {
+	ctx := context.Background()
+	ctx = WithRequestID(ctx, testUUID)
+
+	logger := log.NewLogger()
+	logger.SetFormatter(log.JSONFormat{})
+	buf := new(bytes.Buffer)
+	logger.SetOutput(buf)
+
+	cmd := CommandContext(ctx, "true", "1", "2")
+	cmd.Severity = log.LvDebug
+	cmd.Logger = logger
+	err := cmd.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(buf.Bytes()) > 0 {
+		t.Error(`log should not be recorded`)
+	}
+
+	cmd = CommandContext(ctx, "true", "1", "2")
+	cmd.Severity = log.LvInfo
+	cmd.Logger = logger
+	err = cmd.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var execlog ExecLog
+	err = json.Unmarshal(buf.Bytes(), &execlog)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if time.Since(execlog.LoggedAt) > time.Minute {
+		t.Error(time.Since(execlog.LoggedAt) > time.Minute)
+	}
+	if execlog.Severity != "info" {
+		t.Error(`execlog.Severity != "info"`)
+	}
+	if execlog.Type != "exec" {
+		t.Error(`execlog.Type != "exec"`)
+	}
+	if execlog.Args[1] != "1" {
+		t.Error(`execlog.Args[1] != "1"`)
+	}
+	if execlog.RequestID != testUUID {
+		t.Error(`execlog.RequestID != testUUID`)
+	}
+}
+
+func TestLogCmdError(t *testing.T) {
+	ctx := context.Background()
+	logger := log.NewLogger()
+	logger.SetFormatter(log.JSONFormat{})
+	buf := new(bytes.Buffer)
+	logger.SetOutput(buf)
+
+	cmd := CommandContext(ctx, "/bin/sh", "-c", "echo hoge fuga 1>&2; exit 3")
+	cmd.Severity = log.LvDebug
+	cmd.Logger = logger
+	err := cmd.Run()
+	if err == nil {
+		t.Fatal("the command should fail")
+	}
+
+	var execlog ExecLog
+	err = json.Unmarshal(buf.Bytes(), &execlog)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if execlog.Severity != "error" {
+		t.Error(`execlog.Severity != "error"`)
+	}
+	if execlog.Type != "exec" {
+		t.Error(`execlog.Type != "exec"`)
+	}
+	t.Log(execlog.Error)
+	if execlog.Stderr != "hoge fuga\n" {
+		t.Error(`execlog.Stderr != "hoge fuga\n"`)
+	}
+}
--- jsonlog.go
+++ jsonlog.go
@@ -30,17 +30,18 @@ type AccessLog struct {
 type RequestLog struct {
 	Topic    string    `json:"topic"`
 	LoggedAt time.Time `json:"logged_at"`
-	Severity string    `json:"severity"`
+	Severity string    `json:"severity"` // "error" if request failed.
 	Utsname  string    `json:"utsname"`
 	Message  string    `json:"message"`
 
 	Type         string    `json:"type"`             // "http"
 	ResponseTime float64   `json:"response_time"`    // floating point number of seconds.
-	StatusCode   int       `json:"http_status_code"` // 200, 404, ...
+	StatusCode   int       `json:"http_status_code"` // 200, 404, 500, ...
 	Method       string    `json:"http_method"`
 	URLString    string    `json:"url"`
 	StartAt      time.Time `json:"start_at"`
 	RequestID    string    `json:"request_id"`
+	Error        string    `json:"error"`
 }
 
 // ExecLog is a struct to decode command execution log from LogCmd.
@@ -48,13 +49,14 @@ type RequestLog struct {
 type ExecLog struct {
 	Topic    string    `json:"topic"`
 	LoggedAt time.Time `json:"logged_at"`
-	Severity string    `json:"severity"`
+	Severity string    `json:"severity"` // "error" if exec failed.
 	Utsname  string    `json:"utsname"`
 	Message  string    `json:"message"`
 
-	Type    string   `json:"type"` // "exec"
-	Command string   `json:"command"`
-	Args    []string `json:"args"`
-	Error   string   `json:"error"`
-	Stderr  string   `json:"stderr"`
+	Type      string   `json:"type"` // "exec"
+	Command   string   `json:"command"`
+	Args      []string `json:"args"`
+	RequestID string   `json:"request_id"`
+	Error     string   `json:"error"`
+	Stderr    string   `json:"stderr"`
 }
